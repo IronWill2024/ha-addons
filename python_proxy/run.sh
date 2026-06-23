bd48055074b1dcfdf21bb7d01fb03eb08c37dabb
Hung <Nguyen>
2026-06-23 16:48:13 +0700

    Initial commit for Python Proxy Add-on

---
+++ python_proxy/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+CONFIG_PATH=/data/options.json
+
+USER=$(jq --raw-output '.auth_user' $CONFIG_PATH)
+PASS=$(jq --raw-output '.auth_pass' $CONFIG_PATH)
+
+# Tạo folder và phân quyền cho tinyproxy
+mkdir -p /var/run/tinyproxy /var/log/tinyproxy /etc/tinyproxy
+chown -R tinyproxy:tinyproxy /var/run/tinyproxy /var/log/tinyproxy /etc/tinyproxy
+
+cat <<EOF > /etc/tinyproxy/tinyproxy.conf
+User tinyproxy
+Group tinyproxy
+Port 8888
+Timeout 600
+DefaultErrorFile "/usr/share/tinyproxy/default.html"
+StatFile "/usr/share/tinyproxy/stats.html"
+Logfile "/var/log/tinyproxy/tinyproxy.log"
+LogLevel Info
+PidFile "/var/run/tinyproxy/tinyproxy.pid"
+MaxClients 100
+Allow 0.0.0.0/0
+BasicAuth ${USER} ${PASS}
+EOF
+
+exec /usr/bin/tinyproxy -d
