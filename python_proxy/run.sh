8a6860b18f9d26143e10db462c98b88aeead571f
Hung <Nguyen>
2026-06-23 16:54:18 +0700

    feat: Add log_level config and output logs to stdout

--- python_proxy/run.sh
+++ python_proxy/run.sh
@@ -3,6 +3,7 @@ CONFIG_PATH=/data/options.json
 
 USER=$(jq --raw-output '.auth_user' $CONFIG_PATH)
 PASS=$(jq --raw-output '.auth_pass' $CONFIG_PATH)
+LOG_LEVEL=$(jq --raw-output '.log_level // "Info"' $CONFIG_PATH)
 
 # Tạo folder và phân quyền cho tinyproxy
 mkdir -p /var/run/tinyproxy /var/log/tinyproxy /etc/tinyproxy
@@ -15,8 +16,8 @@ Port 8888
 Timeout 600
 DefaultErrorFile "/usr/share/tinyproxy/default.html"
 StatFile "/usr/share/tinyproxy/stats.html"
-Logfile "/var/log/tinyproxy/tinyproxy.log"
-LogLevel Info
+Logfile "/dev/stdout"
+LogLevel ${LOG_LEVEL}
 PidFile "/var/run/tinyproxy/tinyproxy.pid"
 MaxClients 100
 Allow 0.0.0.0/0
